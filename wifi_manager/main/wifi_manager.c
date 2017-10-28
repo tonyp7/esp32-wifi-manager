cd8c9b89863ae8e2641737e5d3e2a48e72e4aee6
tonyp7 <bubbathemaster@gmail.com>
2017-10-28 22:20:21 +0800

    write ip config json to its own buffer

--- wifi_manager/main/wifi_manager.c
+++ wifi_manager/main/wifi_manager.c
@@ -1,3 +1,24 @@
+/*
+Copyright (c) 2017 Tony Pottier
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 /**
  * \file wifi_manager.c
  * \author Tony Pottier
@@ -112,7 +133,7 @@ void wifi_manager_generate_ip_info_json(){
 	ESP_ERROR_CHECK(tcpip_adapter_get_ip_info(TCPIP_ADAPTER_IF_STA, &ip_info));
 
 	const char ip_info_json_format[] = "{\"ip\":\"%s\",\"netmask\":\"%s\",\"gw\":\"%s\"}\n";
-	sprintf(accessp_json, ip_info_json_format,
+	sprintf(ip_info_json, ip_info_json_format,
 			ip4addr_ntoa(&ip_info.ip),
 			ip4addr_ntoa(&ip_info.netmask),
 			ip4addr_ntoa(&ip_info.gw));
