1fbccfc2e4065bc51508016c0967cfc0bbce5452
tonyp7 <bubbathemaster@gmail.com>
2017-10-30 19:49:31 +0800

    removed esoteric soft copyright, restart from cJSON 1.4.7

--- wifi_manager/main/json.c
+++ wifi_manager/main/json.c
@@ -1,38 +1,36 @@
 /*
- * @file json.c
- * @brief handles very basic JSON with a minimal footprint on the system
- *
- * This code is a lightly modified version of cJSON. cJSON is licensed under the MIT license:
- * Copyright (c) 2009-2017, Dave Gamble and cJSON contributors
- * Copyright (c) 2013, Esoteric Software
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
- * of the Software, and to permit persons to whom the Software is furnished to do
- * so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
- * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- *
- *
- * @see https://github.com/DaveGamble/cJSON
- *
- */
+@file json.c
+@brief handles very basic JSON with a minimal footprint on the system
+
+This code is a lightly modified version of cJSON 1.4.7. cJSON is licensed under the MIT license:
+Copyright (c) 2009 Dave Gamble
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+@see https://github.com/DaveGamble/cJSON
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include "json.h"
 
-const char firstByteMark[7] = { 0x00, 0x00, 0xC0, 0xE0, 0xF0, 0xF8, 0xFC };
 
 char* json_escape_string(char *str){
 
@@ -40,100 +38,113 @@ char* json_escape_string(char *str){
 }
 
 
+//see: static unsigned char* ensure(printbuffer * const p, size_t needed, const internal_hooks * const hooks) cJSON 1.4.7
+bool json_escape_string2(const unsigned char *input, unsigned char *output_buffer)
+{
+	const unsigned char *input_pointer = NULL;
+	unsigned char *output = NULL;
+	unsigned char *output_pointer = NULL;
+	size_t output_length = 0;
+	/* numbers of additional characters needed for escaping */
+	size_t escape_characters = 0;
 
-char* json_unescape_string(const char *str) {
-	const char* ptr = str + 1;
-	char* ptr2;
-	char* out;
-	int len = 0;
-	unsigned uc, uc2;
-	if (*str != '\"') { /* TODO: don't need this check when called from parse_value, but do need from parse_object */
-		//ep = str;
-		return 0;
-	} /* not a string! */
+	if (output_buffer == NULL)
+	{
+		return false;
+	}
 
-	while (*ptr != '\"' && *ptr && ++len)
-		if (*ptr++ == '\\') ptr++; /* Skip escaped quotes. */
+	/* empty string */
+	if (input == NULL)
+	{
+		//output = ensure(output_buffer, sizeof("\"\""), hooks);
+		if (output == NULL)
+		{
+			return false;
+		}
+		strcpy((char*)output, "\"\"");
 
-	out = (char*)malloc(sizeof(char) * (len + 1)); /* The length needed for the string, roughly. */
-	if (!out) return 0;
+		return true;
+	}
 
-	ptr = str + 1;
-	ptr2 = out;
-	while (*ptr != '\"' && *ptr) {
-		if (*ptr != '\\')
-			*ptr2++ = *ptr++;
-		else {
-			ptr++;
-			switch (*ptr) {
-			case 'b':
-				*ptr2++ = '\b';
-				break;
-			case 'f':
-				*ptr2++ = '\f';
-				break;
-			case 'n':
-				*ptr2++ = '\n';
-				break;
-			case 'r':
-				*ptr2++ = '\r';
-				break;
-			case 't':
-				*ptr2++ = '\t';
-				break;
-			case 'u': /* transcode utf16 to utf8. */
-				sscanf(ptr + 1, "%4x", &uc);
-				ptr += 4; /* get the unicode char. */
-
-				if ((uc >= 0xDC00 && uc <= 0xDFFF) || uc == 0) break; /* check for invalid.	*/
-
-																	  /* TODO provide an option to ignore surrogates, use unicode replacement character? */
-				if (uc >= 0xD800 && uc <= 0xDBFF) /* UTF16 surrogate pairs.	*/
-				{
-					if (ptr[1] != '\\' || ptr[2] != 'u') break; /* missing second-half of surrogate.	*/
-					sscanf(ptr + 3, "%4x", &uc2);
-					ptr += 6;
-					if (uc2 < 0xDC00 || uc2 > 0xDFFF) break; /* invalid second-half of surrogate.	*/
-					uc = 0x10000 + (((uc & 0x3FF) << 10) | (uc2 & 0x3FF));
-				}
-
-				len = 4;
-				if (uc < 0x80)
-					len = 1;
-				else if (uc < 0x800)
-					len = 2;
-				else if (uc < 0x10000) len = 3;
-				ptr2 += len;
-
-				switch (len) {
-				case 4:
-					*--ptr2 = ((uc | 0x80) & 0xBF);
-					uc >>= 6;
-					/* fallthrough */
-				case 3:
-					*--ptr2 = ((uc | 0x80) & 0xBF);
-					uc >>= 6;
-					/* fallthrough */
-				case 2:
-					*--ptr2 = ((uc | 0x80) & 0xBF);
-					uc >>= 6;
-					/* fallthrough */
-				case 1:
-					*--ptr2 = (uc | firstByteMark[len]);
-				}
-				ptr2 += len;
-				break;
-			default:
-				*ptr2++ = *ptr;
-				break;
-			}
-			ptr++;
+	/* set "flag" to 1 if something needs to be escaped */
+	for (input_pointer = input; *input_pointer; input_pointer++)
+	{
+		if (strchr("\"\\\b\f\n\r\t", *input_pointer))
+		{
+			/* one character escape sequence */
+			escape_characters++;
+		}
+		else if (*input_pointer < 32)
+		{
+			/* UTF-16 escape sequence uXXXX */
+			escape_characters += 5;
 		}
 	}
-	*ptr2 = 0;
-	if (*ptr == '\"') ptr++; /* TODO error handling if not \" or \0 ? */
-	//item->valueString = out;
-	//item->type = Json_String;
-	//return ptr;
-	return out;
+	output_length = (size_t)(input_pointer - input) + escape_characters;
+
+	/* in the original cJSON it is possible to realloc here in case output buffer is too small.
+	 * This is overkill for an embedded system. */
+	output = output_buffer;
+
+	/* no characters have to be escaped */
+	if (escape_characters == 0)
+	{
+		output[0] = '\"';
+		memcpy(output + 1, input, output_length);
+		output[output_length + 1] = '\"';
+		output[output_length + 2] = '\0';
+
+		return true;
+	}
+
+	output[0] = '\"';
+	output_pointer = output + 1;
+	/* copy the string */
+	for (input_pointer = input; *input_pointer != '\0'; (void)input_pointer++, output_pointer++)
+	{
+		if ((*input_pointer > 31) && (*input_pointer != '\"') && (*input_pointer != '\\'))
+		{
+			/* normal character, copy */
+			*output_pointer = *input_pointer;
+		}
+		else
+		{
+			/* character needs to be escaped */
+			*output_pointer++ = '\\';
+			switch (*input_pointer)
+			{
+			case '\\':
+				*output_pointer = '\\';
+				break;
+			case '\"':
+				*output_pointer = '\"';
+				break;
+			case '\b':
+				*output_pointer = 'b';
+				break;
+			case '\f':
+				*output_pointer = 'f';
+				break;
+			case '\n':
+				*output_pointer = 'n';
+				break;
+			case '\r':
+				*output_pointer = 'r';
+				break;
+			case '\t':
+				*output_pointer = 't';
+				break;
+			default:
+				/* escape and print as unicode codepoint */
+				sprintf((char*)output_pointer, "u%04x", *input_pointer);
+				output_pointer += 4;
+				break;
+			}
+		}
+	}
+	output[output_length + 1] = '\"';
+	output[output_length + 2] = '\0';
+
+	return true;
 }
+
--- wifi_manager/main/json.h
+++ wifi_manager/main/json.h
@@ -1,37 +1,34 @@
 /*
- * @file json.h
- * @brief handles very basic JSON with a minimal footprint on the system
- *
- * This code is a lightly modified version of cJSON. cJSON is licensed under the MIT license:
- * Copyright (c) 2009-2017, Dave Gamble and cJSON contributors
- * Copyright (c) 2013, Esoteric Software
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
- * of the Software, and to permit persons to whom the Software is furnished to do
- * so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
- * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- *
- *
- * @see https://github.com/DaveGamble/cJSON
- *
- */
+@file json.h
+@brief handles very basic JSON with a minimal footprint on the system
+
+This code is a lightly modified version of cJSON 1.4.7. cJSON is licensed under the MIT license:
+Copyright (c) 2009 Dave Gamble
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+@see https://github.com/DaveGamble/cJSON
+*/
+
 #ifndef MAIN_JSON_H_
 #define MAIN_JSON_H_
 
 
 char* json_escape_string(char *str);
-char* json_unescape_string(const char *str);
 
 #endif /* MAIN_JSON_H_ */
