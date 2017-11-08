66076edb59880c82ae546934a73f54cbcb7d1340
tonyp7 <bubbathemaster@gmail.com>
2017-11-08 20:47:49 +0800

    replaced images by base64 in css

--- wifi_manager/main/component.mk
+++ wifi_manager/main/component.mk
@@ -3,4 +3,4 @@
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
-COMPONENT_EMBED_FILES := style.css jquery.gz code.js index.html lock.png wifi0.png wifi1.png wifi2.png wifi3.png
+COMPONENT_EMBED_FILES := style.css jquery.gz code.js index.html
