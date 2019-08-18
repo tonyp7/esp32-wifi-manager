556e556065558486aa7029befcd0a2579bcbe200
Tomoyuki Sakurai <y@trombik.org>
2019-08-18 21:16:28 +0900

    remove unnecessary static files in demo code

--- examples/default_demo/main/component.mk
+++ examples/default_demo/main/component.mk
@@ -2,5 +2,3 @@
 # "main" pseudo-component makefile.
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
-
-COMPONENT_EMBED_FILES := style.css jquery.gz code.js index.html
