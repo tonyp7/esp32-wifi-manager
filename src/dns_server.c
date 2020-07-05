954205a5826a8293ab0d9f6ed69addc93957d995
tonyp7 <bubbathemaster@gmail.com>
2020-07-05 12:27:53 +0800

    Update for esp-idf 4.1

--- src/dns_server.c
+++ src/dns_server.c
@@ -39,17 +39,15 @@ Contains the freeRTOS task for the DNS server that processes the requests.
 #include <freertos/event_groups.h>
 #include <esp_system.h>
 #include <esp_wifi.h>
-#include <esp_event_loop.h>
+#include <esp_event.h>
 #include <esp_log.h>
 #include <esp_err.h>
 #include <nvs_flash.h>
-
 #include <lwip/err.h>
 #include <lwip/sockets.h>
 #include <lwip/sys.h>
 #include <lwip/netdb.h>
 #include <lwip/dns.h>
-
 #include <byteswap.h>
 
 #include "wifi_manager.h"
